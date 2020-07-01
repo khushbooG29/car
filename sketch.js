11948665967237ff4811e23d4870715a45915a17
SWASTIK153 <63796729+SWASTIK153@users.noreply.github.com>
2020-07-01 19:14:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var canvas, database;
+var gameState=0,backgrounImg,playerCount=0,form,player,game;
+
+
+function setup(){
+  database = firebase.database();
+  canvas=createCanvas(500,500);
+game= new Game();
+game.getState();
+game.start();
+
+}
+ 
+
+function draw(){
+  background("white");
+
+  
+    drawSprites();
+  
+}
